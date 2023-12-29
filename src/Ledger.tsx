7c6ad31c784379fa3de68458d2d080616317b26a
Sam Edge <guitarfella@gmail.com>
2023-12-29 21:16:38 +1030

    Initial strapi connect

--- src/Ledger.tsx
+++ src/Ledger.tsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
-import {Item, ItemType} from "./db";
+import {Item, ItemType, StrapiHit, StrapiResultSet} from "./db";
 import ItemView from "./ItemView";
 import styles from "./Ledger.module.css";
 import LedgerItem from "./LedgerItem";
 import {Store} from "./Store";
 
 type Props = {
-  db: Item[],
+  db: StrapiResultSet<Item>,
   store: Store,
-  onClickItem: (item: Item) => void,
-  onDoubleClickItem: (item: Item) => void,
+  onClickItem: (item: StrapiHit<Item>) => void,
+  onDoubleClickItem: (item: StrapiHit<Item>) => void,
 }
 
 const Headings = {
@@ -20,44 +20,53 @@ const Headings = {
 }
 
 function Ledger({ db, store, onClickItem, onDoubleClickItem }: Props) {
-  const isCollected = (entry: Item) => store.isCollected(entry.id);
-  const filterByType = (itemType: ItemType) => (item: Item) => item.type === itemType;
+  const isCollected = (entry: StrapiHit<Item>) => store.isCollected(entry.id);
+  const filterByType = (itemType: ItemType) => (item: StrapiHit<Item>) => item.attributes.itemType === itemType;
   const itemTypes = Object.values(ItemType);
-  const countByItemType = (itemType: ItemType) => db.filter(filterByType(itemType)).length;
+  const countByItemType = (itemType: ItemType) => db.data.filter(filterByType(itemType)).length;
   const collectedByItemType = (itemType: ItemType) => db
+    .data
     .filter(filterByType(itemType))
     .map(item => item.id)
     .map(store.getLogEntry)
     .filter(logEntry => logEntry.collected)
     .length;
+  const collections = db.data.reduce((r, c) => {
+    const n = c.attributes.collection.data.attributes.name;
+    r[n] = r[n] || [];
+    r[n].push(c);
+    return r;
+  }, {} as {[key: string]: StrapiHit<Item>[]});
+
+  console.log("Sorted Collections", collections);
 
   return (
     <>
-      {itemTypes.map(itemType => {
+      {Object.entries(collections).map(([collectionName, items]) => {
         return (
-          <div key={itemType}>
-            <h1 className={styles.LedgerHeading}>
-              {Headings[itemType] ?? "missing"}
-            </h1>
-            <span className={styles.LedgerCounter}>
-               [{collectedByItemType(itemType)}/{countByItemType(itemType)}]
-            </span>
-            <div className={styles.LedgerRow}>
-              {db
-                .filter(filterByType(itemType))
-                .map(item =>
-                  <LedgerItem
+          <div key={collectionName}>
+      <h1 className={styles.LedgerHeading}>
+        {collectionName}
+      </h1>
+      <span className={styles.LedgerCounter}>
+                 {/*[{collectedByItemType(itemType)}/{countByItemType(itemType)}]*/}
+              </span>
+      <div className={styles.LedgerRow}>
+        {items
+            // .filter(filterByType(itemType))
+            .map(item =>
+                <LedgerItem
                     key={item.id}
                     data={item}
                     isCollected={isCollected(item)}
                     onClick={() => onClickItem(item)}
                     onDoubleClick={() => onDoubleClickItem(item)}
-                  />
-                )
-              }
-            </div>
-          </div>
-        )
+                />
+            )
+        }
+      </div>
+      </div>
+    )
       })}
     </>
   )
--- src/db.ts
+++ src/db.ts
@@ -1,9 +1,3 @@
-enum SourceType {
-  Challenge = "challenge",
-  Drop = "drop",
-  Shop = "shop",
-}
-
 enum ItemType {
   Mount = "Mount",
   MountArmor = "Horse Armor",
@@ -12,55 +6,90 @@ enum ItemType {
 }
 
 type Item = {
-  id: string,
-  type: ItemType,
-  icon: string,
+  claim?: string,
+  claimDescription?: string,
+  createdAt: string,
+  publishedAt: string,
+  updatedAt: string,
+  itemId: string,
+  itemType: ItemType,
+  magicType: string,
+  icon?: StrapiResult<StrapiMedia>,
   name: string,
   description: string,
+  outOfRotation: boolean,
+  season?: number,
+  transMog: boolean,
+  usableByClass: string[],
+  collection: StrapiResult<Collection>,
 }
 
-type ItemDto = {
-  id: string,
-  type: string,
-  icon: string,
+type StrapiMediaFormats = {
+  thumbnail?: StrapiMediaFormat,
+}
+
+type StrapiMediaFormat = {
+  ext: string,
+  hash: string,
+  height: number,
+  mime: string,
   name: string,
-  description: string,
-  source: string[],
+  path?: string,
+  size: number,
+  url: string,
+  width: number,
 }
 
-type SourceChallenge = {
-  type: SourceType.Challenge,
-  title: string,
-  description: string,
+type StrapiMedia = {
+  alternativeText?: string
+  caption?: string,
+  createdAt: string,
+  ext: string,
+  formats: StrapiMediaFormats,
+  hash: string,
+  height: number,
+  mime: string,
+  name: string,
+  previewUrl?: string,
+  provider: string,
+  provider_metadata?: string,
+  size: number,
+  updatedAt: string,
+  url: string,
+  width: number,
 }
 
-type SourceDrop = {
-  type: SourceType.Drop,
-  boss: string,
-  title: string,
+type Collection = {
+  id: number,
+  name: string,
+  order: number,
+  createdAt: string,
+  publishedAt: string,
+  updatedAt: string,
+  items: StrapiHit<Item[]>,
 }
 
-type SourceShop = {
-  type: SourceType.Shop,
-  boss: string,
-  title: string,
-  price: number,
+type StrapiResult<T> = {
+  data: StrapiHit<T>,
 }
 
-type Source = SourceChallenge | SourceDrop | SourceShop;
+type StrapiResultSet<T> = {
+  data: StrapiHit<T>[],
+  meta?: {},
+}
 
-async function fetchDb(): Promise<Item[]> {
-  const resp = await fetch('db.json');
-  const data: ItemDto[] = await resp.json();
+type StrapiHit<T> = {
+  id: number,
+  attributes: T,
+}
 
-  return data.map(dto => {
-    return {
-      ...dto,
-      type: dto.type as ItemType,
-    }
-  });
+async function fetchDb(): Promise<StrapiResultSet<Item>> {
+  const resp = await fetch('http://localhost:1337/api/items?populate=*');
+  const data: StrapiResultSet<Item> = await resp.json();
+  console.log("Fetched Items", data);
+  return data;
 }
 
 export default fetchDb;
-export { ItemType };
-export type { Item };
+export { ItemType, fetchDb };
+export type { Item, Collection, StrapiHit, StrapiResultSet, StrapiMedia, StrapiMediaFormat, StrapiMediaFormats };
