b8c533fadbc8986862c9ae31b8a82233d27b4404
Sam Edge <guitarfella@gmail.com>
2024-01-04 13:21:00 +1030

    Update margins, padding etc

--- src/Ledger.tsx
+++ src/Ledger.tsx
@@ -5,6 +5,18 @@ import styles from "./Ledger.module.css";
 import LedgerItem from "./LedgerItem";
 import {Store} from "./Store";
 
+function countItemsInCollection(collection: StrapiHit<Collection>): number {
+    return collection.attributes.items?.data.length ?? 0;
+}
+
+function countItemsInCollectionOwned(store: Store, collection: StrapiHit<Collection>): number {
+    return collection.attributes.items?.data.map(item => item.id).filter(store.isCollected).length ?? 0;
+}
+
+function isCollected(store: Store, entry: StrapiHit<Item>): boolean {
+    return store.isCollected(entry.id);
+}
+
 type Props = {
     db: StrapiResultSet<Collection>,
     store: Store,
@@ -13,38 +25,27 @@ type Props = {
 }
 
 function Ledger({db, store, onClickItem, onDoubleClickItem}: Props) {
-    const isCollected = (entry: StrapiHit<Item>) => store.isCollected(entry.id);
-    // const filterByType = (itemType: ItemType) => (item: StrapiHit<Item>) => item.attributes.itemType === itemType;
-    // const itemTypes = Object.values(ItemType);
-    // const countByItemType = (itemType: ItemType) => db.data.filter(filterByType(itemType)).length;
-    // const collectedByItemType = (itemType: ItemType) => db
-    //   .data
-    //   .filter(filterByType(itemType))
-    //   .map(item => item.id)
-    //   .map(store.getLogEntry)
-    //   .filter(logEntry => logEntry.collected)
-    //   .length;
-
     return (
         <>
-            {db.data.map((collection) => {
+            {db.data.map(collection => {
                 return (
-                    <div key={collection.id}>
-                        <h1 className={styles.LedgerHeading}>{collection.attributes.name}</h1>
-                        <span className={styles.LedgerCounter}>
-                 {/*[{collectedByItemType(itemType)}/{countByItemType(itemType)}]*/}
-              </span>
+                    <div className={styles.LedgerGroup} key={collection.id}>
+                        <div className={styles.LedgerGroupHeading}>
+                            <h1 className={styles.LedgerHeading}>{collection.attributes.name}</h1>
+                            <span className={styles.LedgerCounter}>
+                                [{countItemsInCollectionOwned(store, collection)}/{countItemsInCollection(collection)}]
+                            </span>
+                        </div>
                         <div className={styles.LedgerRow}>
                             {(collection.attributes.items?.data ?? []).map(item =>
                                 <LedgerItem
                                     key={item.id}
                                     data={item}
-                                    isCollected={isCollected(item)}
+                                    isCollected={isCollected(store, item)}
                                     onClick={() => onClickItem(item)}
                                     onDoubleClick={() => onDoubleClickItem(item)}
                                 />
-                            )
-                            }
+                            )}
                         </div>
                     </div>
                 )
