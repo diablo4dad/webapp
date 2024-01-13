8a71d4dcb7d758377f8e2b08bf17632510f6c835
Sam Edge <guitarfella@gmail.com>
2024-01-13 12:53:39 +1030

    Add touch event handler

--- src/Ledger.tsx
+++ src/Ledger.tsx
@@ -36,6 +36,29 @@ function generateEditCategoryUrl(collection: StrapiHit<Collection>): string {
     return SERVER_ADDR + "/admin/content-manager/collectionType/api::collection.collection/" + collection.id;
 }
 
+function onTouchStart(handler: () => void) {
+    return (e: React.TouchEvent) => {
+        const touch = e.touches[0] ?? e.changedTouches[0];
+        const x1 = touch.pageX;
+        const y1 = touch.pageY;
+
+        const onTouchEnd = (evt: Event) => {
+            const touchEvent = evt as TouchEvent;
+            const touch = touchEvent.touches[0] ?? touchEvent.changedTouches[0];
+            const x2 = touch.pageX;
+            const y2 = touch.pageY;
+
+            e.target?.removeEventListener('touchend', onTouchEnd);
+
+            if (x1 === x2 && y1 === y2) {
+                handler();
+            }
+        }
+
+        e.target?.addEventListener('touchend', onTouchEnd);
+    }
+}
+
 type Props = {
     db: StrapiResultSet<Collection>,
     store: Store,
@@ -75,8 +98,10 @@ function Ledger({db, store, onClickItem, onDoubleClickItem, view}: Props) {
                     </div>
                     <div className={styles.LedgerRow}>
                         {(collection.attributes.items?.data ?? []).map(item =>
-                            <div className={getClassNamesForItem(item)} onClick={() => onClickItem(item)}
-                                 onDoubleClick={() => onDoubleClickItem(item)}>
+                            <div className={getClassNamesForItem(item)}
+                                 onClick={() => onClickItem(item)}
+                                 onDoubleClick={() => onDoubleClickItem(item)}
+                                 onTouchStart={onTouchStart(() => onDoubleClickItem(item))}>
                                 <img className={styles.ArtifactImage} src={getImageUrlForItem(item)}
                                      alt={item.attributes.name}/>
                                 <div className={styles.ArtifactInfo}>
