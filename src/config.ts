7ea508da94ee41a1f6aed85afc85d34101728d27
Sam Edge <guitarfella@gmail.com>
2024-04-05 20:43:58 +1030

    Fix total collection count, based on filter selection

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 import {DadBase, DadCollectionItem, DEFAULT_ITEM} from "./db";
 
 const SERVER_ADDR = process.env.NODE_ENV === 'production' ? 'https://db.diablo4.dad' : 'http://localhost:1337';
-const SITE_VERSION = '1.4.1'
-const VERSION = {major: 1, minor: 4, revision: 1}
-const LAST_UPDATED = 'March 14th, 2024'
+const SITE_VERSION = '1.4.2'
+const VERSION = {major: 1, minor: 4, revision: 2}
+const LAST_UPDATED = 'April 5th, 2024'
 const DISCORD_INVITE_LINK = 'https://discord.gg/mPRBrU2kYT'
 const MODE = 'static'
 
