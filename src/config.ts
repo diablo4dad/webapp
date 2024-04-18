40b724655a00477c3da34410a94daa44bd4e1b98
Sam Edge <guitarfella@gmail.com>
2024-04-18 21:45:17 +0930

    Bump to v1.5.0

--- src/config.ts
+++ src/config.ts
@@ -2,11 +2,11 @@ import {DadBase, DadCollectionItem, DEFAULT_ITEM} from "./db";
 
 const SERVER_ADDR = process.env.NODE_ENV === 'production' ? 'https://db.diablo4.dad' : 'http://localhost:1337';
 // const SERVER_ADDR = 'https://db.diablo4.dad';
-const SITE_VERSION = '1.4.2'
-const VERSION = {major: 1, minor: 4, revision: 2}
-const LAST_UPDATED = 'April 5th, 2024'
+const SITE_VERSION = '1.5.0'
+const VERSION = {major: 1, minor: 5, revision: 0}
+const LAST_UPDATED = 'April 18th, 2024'
 const DISCORD_INVITE_LINK = 'https://discord.gg/mPRBrU2kYT'
-const MODE = 'live'
+const MODE = process.env.NODE_ENV === 'production' ? 'static' : 'live';
 
 function getDefaultItemFromCollectionItems(collectionItems: DadCollectionItem): DadBase {
     return collectionItems.items[0] ?? DEFAULT_ITEM;
