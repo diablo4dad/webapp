8dd9607a3f4adcaf5139a2fc24f2b43910d0c919
Sam Edge <guitarfella@gmail.com>
2024-04-19 15:02:42 +0930

    Bump to v1.5.1

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,8 @@ import {DadBase, DadCollectionItem, DEFAULT_ITEM} from "./db";
 
 const SERVER_ADDR = process.env.NODE_ENV === 'production' ? 'https://db.diablo4.dad' : 'http://localhost:1337';
 // const SERVER_ADDR = 'https://db.diablo4.dad';
-const SITE_VERSION = '1.5.0'
-const VERSION = {major: 1, minor: 5, revision: 0}
+const SITE_VERSION = '1.5.1'
+const VERSION = {major: 1, minor: 5, revision: 1}
 const LAST_UPDATED = 'April 18th, 2024'
 const DISCORD_INVITE_LINK = 'https://discord.gg/mPRBrU2kYT'
 const MODE = process.env.NODE_ENV === 'production' ? 'static' : 'live';
