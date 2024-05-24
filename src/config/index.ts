c2be5c470f4b317ecd837cc2394b0648fb7a3943
Sam Edge <guitarfella@gmail.com>
2024-05-24 15:50:52 +0930

    Collection fixes only

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,10 @@
 import {DadBase, DadCollectionItem, DEFAULT_ITEM} from "../data";
 import {Configuration, ItemGroup, itemGroups} from "../common";
 
-const SERVER_ADDR = process.env.NODE_ENV === 'production' ? 'https://db.diablo4.dad' : 'http://localhost:1337';
-// const SERVER_ADDR = 'https://db.diablo4.dad';
-const SITE_VERSION = '1.6.4'
-const VERSION = {major: 1, minor: 6, revision: 4}
+// const SERVER_ADDR = process.env.NODE_ENV === 'production' ? 'https://db.diablo4.dad' : 'http://localhost:1337';
+const SERVER_ADDR = 'https://db.diablo4.dad';
+const SITE_VERSION = '1.6.5'
+const VERSION = {major: 1, minor: 6, revision: 5}
 const LAST_UPDATED = 'May 24th, 2024'
 const DISCORD_INVITE_LINK = 'https://discord.gg/mPRBrU2kYT'
 const MODE = process.env.NODE_ENV === 'production' ? 'static' : 'live';
