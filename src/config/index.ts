ff0b72b1f52ef61809fd768aaaa6adfaee7cea59
Sam Edge <guitarfella@gmail.com>
2024-06-14 15:15:48 +0930

    Update last update tag

--- src/config/index.ts
+++ src/config/index.ts
@@ -8,7 +8,7 @@ const SERVER_ADDR =
 // const SERVER_ADDR = 'https://db.diablo4.dad';
 const SITE_VERSION = "1.6.7";
 const VERSION = { major: 1, minor: 6, revision: 7 };
-const LAST_UPDATED = "June 13th, 2024";
+const LAST_UPDATED = "June 14th, 2024";
 const DISCORD_INVITE_LINK = "https://discord.gg/mPRBrU2kYT";
 const MODE = process.env.NODE_ENV === "production" ? "static" : "live";
 
