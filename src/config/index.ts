a148883db6daa33c364fe0834825a7b47c550713
Sam Edge <guitarfella@gmail.com>
2024-06-14 19:52:35 +0930

    Format config vars

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,13 +1,9 @@
-const SERVER_ADDR =
-  process.env.NODE_ENV === "production"
-    ? "https://db.diablo4.dad"
-    : "http://localhost:1337";
-// const SERVER_ADDR = 'https://db.diablo4.dad';
-const SITE_VERSION = "1.6.7";
 const VERSION = { major: 1, minor: 6, revision: 7 };
+const SITE_VERSION = `${VERSION.major}.${VERSION.minor}.${VERSION.revision}`;
 const LAST_UPDATED = "June 14th, 2024";
 const DISCORD_INVITE_LINK = "https://discord.gg/mPRBrU2kYT";
 const MODE = process.env.NODE_ENV === "production" ? "static" : "live";
+const SERVER_ADDR = "http://localhost:1337";
 
 export {
   VERSION,
