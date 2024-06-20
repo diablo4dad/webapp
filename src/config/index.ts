b6a37f0318fdc8699cd45f661f0bde77c5c9f6e2
Sam Edge <guitarfella@gmail.com>
2024-06-21 08:27:23 +0930

    Update last updated meta

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 const VERSION = { major: 1, minor: 6, revision: 9 };
 const SITE_VERSION = `${VERSION.major}.${VERSION.minor}.${VERSION.revision}`;
-const LAST_UPDATED = "June 14th, 2024";
+const LAST_UPDATED = "June 21st, 2024";
 const DISCORD_INVITE_LINK = "https://discord.gg/mPRBrU2kYT";
 const MODE = process.env.NODE_ENV === "production" ? "static" : "live";
 const SERVER_ADDR = "http://localhost:1337";
