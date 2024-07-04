2aca1edd3cc90f128c5eaabaf2a5cdfadca051fd
Sam Edge <guitarfella@gmail.com>
2024-07-04 15:44:39 +0930

    Refactor shell class names

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -2,23 +2,32 @@ import styles from "./Shell.module.css";
 import React, { ReactNode } from "react";
 
 type Props = {
-  header: ReactNode;
-  sidebar: ReactNode;
-  main: ReactNode;
-  footerSticky: ReactNode;
-  footer: ReactNode;
+  header?: ReactNode;
+  stickyTop?: ReactNode;
+  sidebar?: ReactNode;
+  main?: ReactNode;
+  stickyBottom?: ReactNode;
+  footer?: ReactNode;
 };
 
-function Shell({ header, sidebar, main, footerSticky, footer }: Props) {
+function Shell({
+  header,
+  stickyTop,
+  sidebar,
+  main,
+  stickyBottom,
+  footer,
+}: Props) {
   return (
-    <div className={styles.Page}>
-      <div className={styles.PageHeader}>{header}</div>
-      <div className={styles.PageContent}>
+    <div className={styles.Block}>
+      <div className={styles.Header}>{header}</div>
+      <div className={styles.StickyTop}>{stickyTop}</div>
+      <div className={styles.Content}>
         <aside className={styles.Sidebar}>{sidebar}</aside>
-        <main className={styles.Content}>{main}</main>
+        <main className={styles.Main}>{main}</main>
       </div>
-      <div className={styles.ProgressMobile}>{footerSticky}</div>
-      <div className={styles.Footer}>{footer}</div>
+      <div className={styles.StickyBottom}>{stickyBottom}</div>
+      <footer className={styles.Footer}>{footer}</footer>
     </div>
   );
 }
