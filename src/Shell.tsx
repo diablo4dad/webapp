64d0604f1c290cb13d2b914f4842a52d9b89ab8a
Sam Edge <guitarfella@gmail.com>
2024-07-04 14:56:52 +0930

    Move layout into shell component

--- src/Shell.tsx
+++ src/Shell.tsx
@@ -1,7 +1,28 @@
-function ShellHeader() {
-  return <div></div>;
+import styles from "./Application.module.css";
+import React, { ReactNode } from "react";
+
+type Props = {
+  header: ReactNode;
+  sidebar: ReactNode;
+  main: ReactNode;
+  footerSticky: ReactNode;
+  footer: ReactNode;
+};
+
+function Shell({ header, sidebar, main, footerSticky, footer }: Props) {
+  return (
+    <div className={styles.Page}>
+      <div className={styles.PageHeader}>{header}</div>
+      <div className={styles.PageContent}>
+        <div className={styles.Shell}>
+          <aside className={styles.Sidebar}>{sidebar}</aside>
+          <main className={styles.Content}>{main}</main>
+        </div>
+      </div>
+      <div className={styles.ProgressMobile}>{footerSticky}</div>
+      <div className={styles.Footer}>{footer}</div>
+    </div>
+  );
 }
 
-function ShellContent() {
-  return <div></div>;
-}
+export default Shell;
