f5318b4cd2a0c7a4f509b9716d6a93cbf1665f10
Sam Edge <guitarfella@gmail.com>
2024-11-15 09:30:10 +1030

    Add workarounds for jest tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// > ReferenceError: TextDecoder is not defined
+// TextDecoder is not supported by jsdom; the following is a workaround
+// see: https://github.com/jsdom/jsdom/issues/2524#issuecomment-897707183
+import { TextDecoder, TextEncoder } from "util";
+
+global.TextEncoder = TextEncoder;
+// @ts-ignore
+global.TextDecoder = TextDecoder;
