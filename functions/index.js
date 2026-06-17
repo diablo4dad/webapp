98cf1c462b53e5852f1379023b1680b42ff4ea8e
Sam Edge <guitarfella@gmail.com>
2026-06-17 21:03:35 +0930

    add region to functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,8 @@
 const admin = require("firebase-admin");
 const functions = require("firebase-functions");
+const { setGlobalOptions } = require("firebase-functions/v2");
+
+setGlobalOptions({ region: "us-central1" });
 
 admin.initializeApp();
 
